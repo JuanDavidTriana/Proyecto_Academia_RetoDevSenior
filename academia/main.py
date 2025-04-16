19656f0f390ca94ee11561c404769c52332fe86c
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2025-04-15 22:28:38 -0500

    Agrega la importación del menú de horarios y actualiza el punto de entrada para incluir la gestión de horarios, comentando la línea del menú de cursos. Se inserta una nueva consulta SQL para agregar datos en la tabla de horarios.

--- academia/main.py
+++ academia/main.py
@@ -1,6 +1,7 @@
 from view.menu import menu_principal
 from view.menuDocente import menu_principal_docente
 from view.menuCurso import menu_principal_curso
+from view.menuHorario import menu_principal_horario
 from config.database import Database
 
 if __name__ == "__main__":
@@ -8,7 +9,8 @@ if __name__ == "__main__":
     try:
         #menu_principal(db) Menu de estudiantes
         #menu_principal_docente(db) #Menu de docentes
-        menu_principal_curso(db) #Menu de cursos
+        #menu_principal_curso(db) #Menu de cursos
+        menu_principal_horario(db) #Menu de horarios
         
     finally:
         db.close()
