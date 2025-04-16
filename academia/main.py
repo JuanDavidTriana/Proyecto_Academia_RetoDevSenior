dfb93abd799709ad5d45581bf3df0e526bb8d6d0
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2025-04-15 21:30:38 -0500

    Modifica el punto de entrada para utilizar el menú de docentes en lugar del menú de estudiantes, comentando la línea anterior. Esto permite gestionar las opciones específicas para docentes.

--- academia/main.py
+++ academia/main.py
@@ -1,9 +1,11 @@
 from view.menu import menu_principal
+from view.menuDocente import menu_principal_docente
 from config.database import Database
 
 if __name__ == "__main__":
     db = Database()
     try:
-        menu_principal(db)
+        #menu_principal(db) Menu de estudiantes
+        menu_principal_docente(db) #Menu de docentes
     finally:
         db.close()
